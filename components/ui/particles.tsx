6eff46fbf484785c2d58c4ee862fddd78fa10532
NandoG1 <fernandogunawan291105@gmail.com>
2025-05-13 15:29:14 +0700

    change canva

--- components/ui/particles.tsx
+++ components/ui/particles.tsx
@@ -34,8 +34,8 @@ export function ParticleBackground() {
       color: string
 
       constructor() {
-        this.x = Math.random() * canvas.width
-        this.y = Math.random() * canvas.height
+        this.x = Math.random() * (canvas?.width ?? window.innerWidth)
+        this.y = Math.random() * (canvas?.height ?? window.innerHeight)
         this.size = Math.random() * 3 + 1
         this.speedX = (Math.random() - 0.5) * 0.5
         this.speedY = (Math.random() - 0.5) * 0.5
@@ -46,10 +46,13 @@ export function ParticleBackground() {
         this.x += this.speedX
         this.y += this.speedY
 
-        if (this.x > canvas.width) this.x = 0
-        else if (this.x < 0) this.x = canvas.width
-        if (this.y > canvas.height) this.y = 0
-        else if (this.y < 0) this.y = canvas.height
+        const width = canvas?.width ?? window.innerWidth
+        const height = canvas?.height ?? window.innerHeight
+
+        if (this.x > width) this.x = 0
+        else if (this.x < 0) this.x = width
+        if (this.y > height) this.y = 0
+        else if (this.y < 0) this.y = height
       }
 
       draw() {
